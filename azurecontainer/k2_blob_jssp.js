1e35a3bcb83ef0e9d066e0d5a1768f2e3f618472
jasonr31 <112577090+jasonr31@users.noreply.github.com>
2025-10-28 17:29:19 -0400

    Update k2_blob_jssp.js

--- azurecontainer/k2_blob_jssp.js
+++ azurecontainer/k2_blob_jssp.js
@@ -3,6 +3,16 @@ metadata = {
   displayName: "MROH File Download Broker",
   description: "A broker for downloading files from MROH Azure Blob Storage.",
   configuration: {
+    storageAccountUrl: {
+      displayName: "Storage Account URL",
+      type: "string",
+      value: "https://mrohstoragetechrefdev01.blob.core.windows.net"
+    },
+    containerPath: {
+      displayName: "Container Path",
+      type: "string",
+      value: "/mrohstoragetechrefdev01/Technical%20Reference/Work%20Orders/2025-298/2025-0394872/References"
+    },
     "x-ms-version": {
       displayName: "Azure Storage API Version",
       type: "string",
@@ -10,117 +20,89 @@ metadata = {
     }
   }
 };
-const m = "https://mrohstoragetechrefdev01.blob.core.windows.net", f = "/mrohstoragetechrefdev01/Technical%20Reference/Work%20Orders/2025-298/2025-0394872/References", u = "file", p = "content", d = "fileName", a = "path", h = "download";
-ondescribe = async function({ configuration: s }) {
-  const o = {
+ondescribe = async function({ configuration: t }) {
+  postSchema({
     objects: {
-      [u]: {
+      file: {
         displayName: "File",
         description: "Download file content from Azure Blob Storage",
         properties: {
-          [p]: {
+          content: {
             displayName: "File Content",
             type: "string"
           },
-          [d]: {
+          fileName: {
             displayName: "File Name",
             type: "string"
           }
         },
         methods: {
-          [h]: {
+          download: {
             displayName: "Download File",
             type: "read",
-            outputs: [p, d],
+            outputs: ["content", "fileName"],
             parameters: {
-              [a]: {
+              path: {
                 displayName: "File Path",
                 description: "Path to the file in blob storage",
                 type: "string"
               }
             },
-            requiredParameters: [a]
+            requiredParameters: ["path"]
           }
         }
       }
-    },
-    // Expose service key properties so they appear in the K2 Service Instance UI.
-    // K2 will surface these for users to set when creating a service instance.
-    configuration: {
-      serviceKeyProperties: {
-        "x-ms-version": {
-          displayName: "Azure Storage API Version (x-ms-version)",
-          description: "Optional override for the x-ms-version header sent to Azure Storage",
-          type: "string",
-          default: "2020-04-08"
-        }
-      }
     }
-  };
-  postSchema(o);
+  });
 };
 onexecute = async function({
-  objectName: s,
-  methodName: o,
-  parameters: i,
-  properties: e,
-  configuration: r
+  objectName: t,
+  methodName: n,
+  parameters: r,
+  properties: o,
+  configuration: a
 }) {
-  switch (s) {
-    case u:
-      await y(o, i, e, r);
+  switch (t) {
+    case "file":
+      await h(n, r, o, a);
       break;
     default:
-      throw new Error("The object " + s + " is not supported.");
+      throw new Error("The object " + t + " is not supported.");
   }
 };
-async function y(s, o, i, e) {
-  switch (s) {
-    case h:
-      await w(o, i, e);
+async function h(t, n, r, o) {
+  switch (t) {
+    case "download":
+      await u(n, r, o);
       break;
     default:
-      throw new Error("The method " + s + " is not supported.");
+      throw new Error("The method " + t + " is not supported.");
   }
 }
-function w(s, o, i) {
-  return new Promise((e, r) => {
-    if (typeof s[a] != "string")
-      throw new Error('parameters["path"] is not of type string');
-    s[a], v(s, o, i, (t, n) => {
-      if (t) return r(t);
-      postResult(n), e();
-    });
-  });
-}
-function v(s, o, i, e) {
-  const r = s[a], t = `${m}${f}/${r}`;
-  x(t, "", "GET", i, (n, l) => {
-    if (n) return e(n);
-    const c = r.includes("/") && r.split("/").pop() || r;
-    console.log("URL: " + t), e(null, {
-      [p]: l,
-      [d]: c
-    });
-  });
-}
-function x(s, o, i, e, r) {
-  var t = new XMLHttpRequest();
-  t.withCredentials = !0, t.onreadystatechange = function() {
-    try {
-      if (t.readyState !== 4) return;
-      if (t.status !== 200)
-        throw new Error("Failed with status " + t.status);
-      r(null, t.responseText);
-    } catch (c) {
-      r(c);
+function u(t, n, r) {
+  return new Promise(function(o, a) {
+    if (typeof t.path != "string") {
+      a(new Error('parameters["path"] is not of type string'));
+      return;
     }
-  }, t.open(i, s);
-  let n;
-  try {
-    e && typeof e.xMsVersion == "string" ? n = e.xMsVersion : e && e.serviceKeyProperties && typeof e.serviceKeyProperties["x-ms-version"] == "string" ? n = e.serviceKeyProperties["x-ms-version"] : e && e.serviceProperties && typeof e.serviceProperties["x-ms-version"] == "string" && (n = e.serviceProperties["x-ms-version"]);
-  } catch {
-  }
-  const l = n || "2020-04-08";
-  t.setRequestHeader("x-ms-version", l), console.log("Request Header: " + l), t.send(o), console.log("ExecuteRequest error:" + t.responseText);
+    const c = r.storageAccountUrl || "https://mrohstoragetechrefdev01.blob.core.windows.net", l = r.containerPath || "/mrohstoragetechrefdev01/Technical%20Reference/Work%20Orders/2025-298/2025-0394872/References", s = t.path, d = c + l + "/" + s, p = r["x-ms-version"] || "2020-04-08";
+    var e = new XMLHttpRequest();
+    e.withCredentials = !0, e.onreadystatechange = function() {
+      try {
+        if (e.readyState !== 4)
+          return;
+        if (e.status !== 200) {
+          a(new Error("Failed with status " + e.status + ": " + e.statusText));
+          return;
+        }
+        const i = s.includes("/") ? s.split("/").pop() : s;
+        postResult({
+          content: e.responseText,
+          fileName: i
+        }), o();
+      } catch (i) {
+        a(i);
+      }
+    }, e.open("GET", d), e.setRequestHeader("x-ms-version", p), e.send();
+  });
 }
